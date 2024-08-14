2133017a2947b9d8bc74652442a4078a03cc54cb
Nazzey24 <89928171+Nazzey24@users.noreply.github.com>
2024-08-14 11:24:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const navContainer = document.getElementById('nav-container');
+
+fetch('nav.html')
+  .then(response => response.text())
+  .then(data => {
+    navContainer.innerHTML = data;
+  });
 
